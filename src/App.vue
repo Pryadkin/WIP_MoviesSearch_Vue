2852064156d93ebbde7541e04df8aa8d05a41b42
Pryadkin Anton <a.pryadkin@k50dev.ru>
2021-04-15 15:05:54 +0300

    add routing to Home, Search

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <nav>
+      <div class="nav-wrapper brown darken-1">
+        <div class="layout">
+          <a href="#!" class="brand-logo">Logo</a>
+          <ul class="right hide-on-med-and-down">
+            <li><router-link to="/"><i class="material-icons right">view_module</i>Home</router-link></li>
+            <li><router-link to="/search"><i class="material-icons left">search</i>Search movies</router-link></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import '~materialize-css/dist/css/materialize.min.css';
+
+.nav-wrapper > .layout {
+  padding: 0 20px;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/views/Seatch.vue
+++ src/views/Seatch.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="home">
-    Home
+  <div class="search">
+    Search
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'Home'
+  name: 'Search'
 }
 </script>
